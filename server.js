c4478b412fd23a7bcf3042584ece14384273e547
Gà <nghquan89@gmail.com>
2017-01-01 12:43:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -69,15 +69,11 @@ function sendMessage(senderId, message) {
   });
 }
 
-// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('port', process.env.PORT || 3002);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
-server.listen(port, function () {
-	console.log('started');
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
 });
-// server.listen(app.get('port'), app.get('ip'), function() {
-//   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
-// });
 
 
