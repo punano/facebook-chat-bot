c07b81438ced0f548f45e63d99aec09a04d74e24
Gà <nghquan89@gmail.com>
2017-01-06 09:43:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({
 }));
 var server = http.createServer(app);
 
+var SimsimiAnswered;
+var text;
+var botkey = "http://www.simsimi.com/getRealtimeReq?uuid=UwmPMKoqosEETKleXWGOJ6lynN1TQq18wwvrmCy6IRt&lc=vn&ft=1&reqText=";
+// var botkey = "http://www.simsimi.com/getRealtimeReq?uuid=m0njJQ6vh8ElgCfIsaZ6Zp8yYoZ0O1szQWaIvPOlpXg&lc=vi&ft=0&reqText="; // Key thay thế
+// mục botkey, có đoạn ft=0 (tức là không lọc chát - sẽ chát cả từ thô tục), ft=1 là để lọc 
 
 app.get('/', (req, res) => {
   res.send("Home page. Server running okay.");
@@ -39,8 +44,7 @@ app.post('/webhook', function(req, res) {
         // If user send text
         if (message.message.text) {
           var text = message.message.text;
-          console.log(text); // In tin nhắn người dùng
-          sendMessage(senderId, "Tui là bot đây: " + text);
+          simsimiBot(senderId, text)
         }
       }
     }
@@ -49,13 +53,53 @@ app.post('/webhook', function(req, res) {
   res.status(200).send("OK");
 });
 
+function simsimiBot(senderId, message){
+  //   if(message.body === "/logout") { //khi nhận được tin nhắn có nội dung "/logout" thì bot sẽ logout khỏi facebook, bot sẽ dừng
+		// 	sendMessage(" Sim Trả Lời:\nSim đi ngủ nhé, tạm biệt ạ.", message.threadID); // auto gửi tin nhắn đi đọc
+		// }
+		if (message==="Getid"||message==="getid"||message==="get id"||message==="Get id") {
+			console.log("FromID: " + senderId + '->Message: '+message);
+			sendMessage(senderId, "Your ID: "+ senderId); // auto gửi tin nhắn đio
+		} else if (senderId==="id_loại_trừ_1"||senderId=="id_loại_trừ_2") {
+		// id_loại_trừ: thêm id người dùng mà bot không tự trả lời
+		// hiện tại mình để cho 2 id, có thể viết thêm theo cấu trúc như trên			 
+			console.log("FromID: " + senderId + '->Message: '+message);
+			return;
+		} else if (senderId==="184857308369687") {
+			console.log("FromID: " + senderId + '->Message: '+message);
+			return;
+		} else if (message)
+		{
+			console.log("FromID: " + senderId + '->Message: '+message);
+			request(botkey + encodeURI(message),  // gửi tin nhắn của người dùng lên máy chủ Simsimi
+			function(error, response, body)
+			{    // trả về tin nhắn khi lấy từ máy chủ Simsimi
+				if (error) sendMessage(senderId, "Message bot: Đang đơ, không trả lời được :)"); //trả về tin nhắn khi thất bại
+				if (body.indexOf("502 Bad Gateway") > 0 || body.indexOf("respSentence") < 0)
+					sendMessage(senderId, "Bot Message: Viết gì khó hiểu vl..."); //trả về tin nhắn khi thất bại
+				
+				text = JSON.parse(body);
+				if (text.status == "200")
+				{
+					SimsimiAnswered = text.respSentence; // trả về tin nhắn được lấy thành công
+					if (message===text.respSentence) {
+						return;
+					} else
+					SimsimiAnswered = text.respSentence; // trả về tin nhắn được lấy thành công
+					sendMessage(senderId,"Bot Message: "+SimsimiAnswered);
+					console.log("Answered:"+SimsimiAnswered);
+				}
+			});
+		}
+}
+
 
 // Gửi thông tin tới REST API để trả lời
 function sendMessage(senderId, message) {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
-      access_token: "EAACOpZBHywK8BAOjpQ4I8JflQN22qARQEoJY0VZC9a42aIVLjjYnnExr3FUtFagWaLvzXNyBNFc2uw5Yt09P4pZABdaPLzEZAIb1wUTkQ7yQnMb6SzXZApI03jHN1JivvwbLfv7tQ6qwZAaIz2ZCgDu9gVtW7jip30IdzoCNzsowQZDZD",
+      access_token: "EAACOpZBHywK8BAGdFF0ic6krVXwuLr6Ui6l0bEncvXcQE1XVAMTNZA0I35dsxGvypkFVlXVdMDV8QZCUODWGbTTyPuXtKpa37Fp9QSrFxzWIpSp6YsKQxLRTg3P8VWWby83kDxwRHKGSiDLvbsl4ObJ6LgXT70lUqXhoZAeeZBgZDZD",
     },
     method: 'POST',
     json: {
@@ -69,13 +113,6 @@ function sendMessage(senderId, message) {
   });
 }
 
-// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-// app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
-var server_port =  process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 50000;
-var server_host = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1';
-
-server.listen(server_port , server_host, function() {
-  console.log("Chat bot server listening at %s:%d ", server_port, server_host);
+server.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Chat bot server listening at %s:%d ", process.env.IP, process.env.PORT);
 });
-
-
